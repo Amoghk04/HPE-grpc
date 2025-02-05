b5b39522d1e9c5119fb040b84650ed362b6bed09
Amoghk04 <kalasapuraamogh@gmail.com>
2025-02-05 14:48:14 +0530

    gRPC: Added secure connection (TLS/SSL)

--- grpc/client.cpp
+++ grpc/client.cpp
@@ -2,6 +2,7 @@
 #include <memory>
 #include <grpcpp/grpcpp.h>
 #include "service.grpc.pb.h"
+#include <fstream>
 
 using grpc::Channel;
 using grpc::ClientContext;
@@ -14,17 +15,21 @@ using namespace std;
 class GreeterClient {
     public:
         GreeterClient(shared_ptr<Channel> channel) : stub_(Greeter::NewStub(channel)) {}
+
         string SayHello(const string& name) {
             HelloRequest request;
             request.set_name(name);
             HelloReply reply;
             ClientContext context;
 
+            cout << "[CLIENT] Sending request to server: " << name << endl;
             Status status = stub_->SayHello(&context, request, &reply);
 
             if (status.ok()) {
+                cout << "[CLIENT] Server response received: " << reply.message() << endl;
                 return reply.message();
             } else {
+                cerr << "[CLIENT] RPC failed: " << status.error_message() << endl;
                 return "RPC failed";
             }
         }
@@ -32,12 +37,39 @@ class GreeterClient {
         unique_ptr<Greeter::Stub> stub_;
 };
 
+string read_file(const string& filename) {
+    ifstream file(filename, ios::binary);
+    if (!file) {
+        cerr << "[CLIENT] Error: Could not open file " << filename <<endl;
+        exit(1);
+    }
+    return string((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+}
+
 int main() {
-    GreeterClient client(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
+    string root_cert = read_file("ca.crt");
+    string client_cert = read_file("client.crt");
+    string client_key = read_file("client.key");
+
+    cout << "[CLIENT] Loaded SSL certificates successfully!" << endl;
+
+    grpc::SslCredentialsOptions ssl_opts;
+    ssl_opts.pem_root_certs = root_cert;
+    ssl_opts.pem_private_key = client_key;
+    ssl_opts.pem_cert_chain = client_cert;
+
+    auto channel_creds = grpc::SslCredentials(ssl_opts);
+    auto channel = grpc::CreateChannel("localhost:50051", channel_creds);
+    cout << "[CLIENT] Connected to server using SSL🚀" << endl;
+
+    GreeterClient client(channel);
+
     string my_name;
     cout << "Enter your name: " << endl;
     cin >> my_name;
+    
     string response = client.SayHello(my_name);
-    cout << "Server Response: " << response << endl;
+    cout << "[CLIENT] Final Response: " << response << endl;
+    
     return 0;
 }
